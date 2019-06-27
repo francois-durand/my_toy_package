bc6ea9b88295eef23daacbcf8ba244d9aa23c64c
francois-durand <fradurandpub@gmail.com>
2019-06-27 10:58:53 +0200

    Prepare v0.3.2

--- my_toy_package/__init__.py
+++ my_toy_package/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """François Durand"""
 __email__ = 'fradurand@gmail.com'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 from .SubPackage1.MyClass1 import MyClass1
 from .SubPackage2.MyClass2 import MyClass2
