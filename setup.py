f08f8c201347f81fffa19bdb799afa949bc3ba9c
francois-durand <fradurandpub@gmail.com>
2019-12-19 15:40:32 +0100

    Release 0.4.0

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/francois-durand/my_toy_package',
-    version='0.3.2',
+    version='0.4.0',
     zip_safe=False,
 )
