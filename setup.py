e875f3cb9051fbfd0cf0395d8680bfcc416bb7d4
francois-durand <fradurandpub@gmail.com>
2020-01-22 18:22:44 +0100

    Bump version: 0.8.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/francois-durand/my_toy_package',
-    version='0.8.0',
+    version='0.9.0',
     zip_safe=False,
 )
